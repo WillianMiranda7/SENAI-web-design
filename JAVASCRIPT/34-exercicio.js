9a228b6e44ead6022733ff4faaa9fa6c693cbd27
WillianMiranda7 <willianmiranda243@gmail.com>
2024-04-13 23:13:37 -0300

    13/04/2024 - home

--- JAVASCRIPT/34-exercicio.js
+++ JAVASCRIPT/34-exercicio.js
@@ -1,26 +1,42 @@
 
 function resultado() {
-    let local = Number(document.getElementById("pontuacaoLocal").value)
-    let visitante = Number(document.getElementById("pontuacaoVisitante").value)
+    let local = document.getElementById("pontuacaoLocal").value.trim()
+    let visitante = document.getElementById("pontuacaoVisitante").value.trim()
     let resposta = document.getElementById("resposta")
+    let localNum = local === ""? NaN : Number(local)
+    let visitanteNum = visitante === ""? NaN :  Number(visitante)
+    let vitoriaLocal = localNum > visitanteNum
+    let vitoriaVisitante = visitanteNum > localNum
+    let empate = localNum == visitanteNum
+    let invalido = isNaN(localNum) || isNaN(visitanteNum)
 
-    console.log(local)
-    console.log(visitante)
-
-    // Se os times empatarem 
-    if (local === "" || visitante === "") {
-        resposta.innerText = "Digite todos os valores"
-    }
-    // Se o time local vencer
-    else if (local > visitante) {
-        resposta.innerText = "Vitória do time local!"
-    }
-    // Se o time local vencer
-    else if (visitante > local) {
-        resposta.innerText = "Vitória do time visitante!"
-    }
-    // Se o time visitante vencer
-    else if (local === visitante) {            // Se não digitar nenhum valor
-        resposta.innerText = "Houve um empate!"
+    if (invalido) {
+        resposta.textContent = "Digite todos os valores"
+    } else if (empate) {
+        resposta.textContent = "Houve um empate!"
+    } else if (vitoriaLocal) {
+        resposta.textContent = "Vitória do time local!"
+    } else if (vitoriaVisitante) {
+        resposta.textContent = "Vitória do time visitante!"
     }
 }
+
+
+
+
+// // Se os times empatarem
+// if (local === "" || visitante === "") {
+//     resposta.innerText = "Digite todos os valores"
+// }
+// // Se o time local vencer
+// else if (local > visitante) {
+//     resposta.innerText = "Vitória do time local!"
+// }
+// // Se o time local vencer
+// else if (visitante > local) {
+//     resposta.innerText = "Vitória do time visitante!"
+// }
+// // Se o time visitante vencer
+// else if (local === visitante) {            // Se não digitar nenhum valor
+//     resposta.innerText = "Houve um empate!"
+// }
